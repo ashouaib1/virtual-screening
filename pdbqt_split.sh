e7054ac8b833b2e831a6f39880b15b7ec2fb258b
Abdullah Danish Shouaib <ashouaib1@student.gsu.edu>
2019-05-13 19:09:36 -0400

    Updated pdbqt_split.sh
    There was a bug in the original script that would overlook a great deal of pdbqt files, which led to a reduction of sample files. This was because the original script called on the autodock program to split up the files. The new script no longer relies on autodock and uses simple logic to split the input files into the necessary chunks for the virtual screening.

--- pdbqt_split.sh
+++ pdbqt_split.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
 mkdir ./lig_files/
-gunzip ./dump/*/*/*.gz
+cp ./dump/*/*/*.gz ./lig_files
+gunzip ./lig_files/*
+cd ./lig_files/
 
-for s in ./*/*/*/*.xaa.pdbqt; do
-    check=$(head -c 6 $s)
-    if [ $check == "REMARK" ]; then
-        mv $s ./lig_files/
-    else
-        vina_split --input $s --ligand ./lig_files/lig
-    fi
+for s in ./*.xaa.pdbqt; do
+    csplit $s '/MODEL/' '{*}'
+    for i in xx*; do
+        zinc=$(cat $i | head -2 | tail -1 | tail -c 17)
+        mv -- "$i" ./"${zinc}.pdbqt"
+    done
 done
 
-#rm ./lig_files/*.xaa.pdbqt
+rm *.xaa.pdbqt
