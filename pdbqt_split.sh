fd80f76198b5eef1f51d4bda48b9058f19c253e7
Abdullah Danish Shouaib <ashouaib1@student.gsu.edu>
2019-05-16 14:07:05 -0400

    Update pdbqt_split.sh

--- pdbqt_split.sh
+++ pdbqt_split.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
 
 mkdir ./lig_files/
-cp ./dump/*/*/*.gz ./lig_files
+cp ./downloads/*/*/*.gz ./lig_files
 gunzip ./lig_files/*
 cd ./lig_files/
 
 for s in ./*.xaa.pdbqt; do
     csplit $s '/MODEL/' '{*}'
     for i in xx*; do
-        zinc=$(cat $i | head -2 | tail -1 | tail -c 17)
-        mv -- "$i" ./"${zinc}.pdbqt"
+        check=$(head -c 6 $i)
+        if [[ $check == "REMARK" ]]; then
+            zinc_id=$(cat $i | head -1 | tail -c 17)
+            mv -- "$i" ./"${zinc_id}.pdbqt"
+        else
+            zinc=$(cat $i | head -2 | tail -1 | tail -c 17)
+            mv -- "$i" ./"${zinc}.pdbqt"
+        fi
     done
 done
 
