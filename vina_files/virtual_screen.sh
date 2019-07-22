b3391a1c6b099b0fec6d3ad790ea733a3a6ccdce
Abdullah Danish Shouaib <ashouaib1@student.gsu.edu>
2019-07-21 20:04:40 -0400

    Update virtual_screen.sh

--- vina_files/virtual_screen.sh
+++ vina_files/virtual_screen.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
-# mkdir results
 
-for f in *.pdbqt; do
+receptor_file='/userdata1/ashouaib/vina_files/cyp_A.pdbqt'
+config_file='/userdata1/ashouaib/vina_files/config.txt'
+
+mkdir results
+
+for f in $(cat $1); do
     b=`basename $f .pdbqt`
-    /software/autodock_vina_1_1_2_linux_x86/bin/vina --config ../../vina_files/config.txt --receptor ../../vina_files/receptor.pdbqt --ligand $f --out ../results/${b}_results.pdbqt --log ../results/log_${b}.log
+    /software/autodock_vina_1_1_2_linux_x86/bin/vina --config $config_file --receptor $receptor_file --cpu 1 --ligand /userdata1/ashouaib/dataset/$f --out results/${b}_vina.pdbqt --log results/${b}_vina.log
 done
+
+echo "SCREENING DONE"
